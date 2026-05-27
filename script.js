ddb1b3a606d5b3f93efcf87de2b5c833aafe3001
michaeladegoke <adegokeolujidemic@gmail.com>
2026-05-27 20:55:03 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+
+  const teachersRow = document.querySelector(".teachers-row");
+  const leftArrow = document.querySelector(".left-arrow");
+  const rightArrow = document.querySelector(".right-arrow");
+
+  const scrollAmount = teachersRow.clientWidth;
+
+  // RIGHT ARROW
+  rightArrow.addEventListener("click", () => {
+    teachersRow.scrollBy({
+      left: scrollAmount,
+      behavior: "smooth"
+    });
+  });
+
+  // LEFT ARROW
+  leftArrow.addEventListener("click", () => {
+    teachersRow.scrollBy({
+      left: -scrollAmount,
+      behavior: "smooth"
+    });
+  });
+
+  // AUTO SLIDE
+  function autoSlide() {
+
+    // if carousel reaches end, go back to start
+    if (
+      teachersRow.scrollLeft + teachersRow.clientWidth
+      >= teachersRow.scrollWidth - 5
+    ) {
+
+      teachersRow.scrollTo({
+        left: 0,
+        behavior: "smooth"
+      });
+
+    } else {
+
+      teachersRow.scrollBy({
+        left: scrollAmount,
+        behavior: "smooth"
+      });
+
+    }
+  }
+
+  // move every 3 seconds
+  setInterval(autoSlide, 3000);
